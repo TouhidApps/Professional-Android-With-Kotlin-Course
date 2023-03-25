db62acc597c51f2d360672262c0e0008477ca146
Touhidul Islam <touhidapps@gmail.com>
2023-03-26 02:17:06 +0800

    update

---
+++ 006_Activity-To-Activity/app/src/main/java/com/touhidapps/activitytoactivity/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.touhidapps.activitytoactivity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnNextPage = findViewById<Button>(R.id.btnNextPage)
+        btnNextPage.setOnClickListener {
+            val i = Intent(this@MainActivity, SecondActivity::class.java)
+            startActivity(i)
+        }
+
+    }
+
+
+
+
+}
