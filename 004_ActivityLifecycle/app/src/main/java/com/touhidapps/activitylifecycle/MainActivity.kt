74b3ee9c15b20bde57cd55549ff451107c845619
Touhidul Islam <touhidapps@gmail.com>
2023-03-25 20:48:16 +0800

    init

---
+++ 004_ActivityLifecycle/app/src/main/java/com/touhidapps/activitylifecycle/MainActivity.kt
@@ -0,0 +1,85 @@
+package com.touhidapps.activitylifecycle
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.touhidapps.activitylifecycle.ui.theme.ActivityLifecycleTheme
+
+class MainActivity : ComponentActivity() {
+
+    private val TAG = "MainActivity_TAG"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ActivityLifecycleTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+
+    //    println("onCreate")
+        Log.d(TAG, "onCreate")
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy")
+    }
+
+} // MainActivity
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ActivityLifecycleTheme {
+        Greeting("Android")
+    }
+}
---
+++ 005_ApplicationClass/app/src/main/java/com/touhidapps/applicationclass/App.kt
@@ -0,0 +1,20 @@
+package com.touhidapps.applicationclass
+
+import android.app.Application
+
+class App: Application() {
+
+    // User data object
+    // Other singleton class if necessary
+
+    override fun onCreate() {
+        super.onCreate()
+
+        // Firebase init
+        // NDK load
+
+
+    }
+
+
+}
