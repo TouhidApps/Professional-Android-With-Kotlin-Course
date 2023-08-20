7aebc17a26bab34d28506e2328a9379627f53ee8
Touhidul Islam <touhidapps@gmail.com>
2023-08-20 17:19:57 +0800

    UI project, webview, event listener added

---
+++ 010_UI-Design-Project/app/src/main/java/com/touhidapps/ui_design_project/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.touhidapps.ui_design_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.material.checkbox.MaterialCheckBox
+
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+      // setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_registration)
+        setContentView(R.layout.activity_article)
+//
+//        val checkBoxAgree = findViewById<MaterialCheckBox>(R.id.checkBoxAgree)
+//
+//        checkBoxAgree.addOnCheckedStateChangedListener { checkBox, state ->
+//            if (checkBox.isChecked) {
+//                checkBox.text = "আপনি সম্মত হয়েছেন।"
+//            } else {
+//                checkBox.text = "I agree"
+//            }
+//        }
+
+
+
+
+    }
+
+
+
+}
