6f0171d1935add3fffc6f70775c03a05281eee26
Touhidul Islam <touhidapps@gmail.com>
2023-09-03 19:10:53 +0800

    notification added

--- 13_Service-And-Notification/app/src/main/java/com/touhidapps/a13_serviceandnotification/MainActivity.kt
+++ 13_Service-And-Notification/app/src/main/java/com/touhidapps/a13_serviceandnotification/MainActivity.kt
@@ -1,16 +1,21 @@
 package com.touhidapps.a13_serviceandnotification
 
-import android.app.Service
-import android.content.ComponentName
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.content.ServiceConnection
-import android.os.Binder
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.BitmapFactory
+import android.graphics.drawable.Drawable
+import android.os.Build
 import android.os.Bundle
-import android.os.IBinder
-import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.graphics.drawable.toBitmap
 import com.touhidapps.a13_serviceandnotification.databinding.ActivityMainBinding
+import kotlin.random.Random
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -32,7 +37,58 @@ class MainActivity : AppCompatActivity() {
             stopService(i)
         }
 
+        binding.btnNotificaiton.setOnClickListener {
 
-    }
+            showNotificaiton()
+
+        }
+
+
+    } // onCreate
+
+
+    private fun showNotificaiton() {
+
+        val intent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+
+        val channelId = "my_channel_id"
+        val bm = (resources.getDrawable(R.mipmap.ic_launcher) as Drawable).toBitmap()
+        val builder = NotificationCompat.Builder(this, channelId).apply {
+            setSmallIcon(R.mipmap.ic_launcher)
+            setLargeIcon(bm)
+            setContentTitle("My Notificaiton")
+            setContentText("Hello Notificaiton!")
+            setStyle(NotificationCompat.BigTextStyle().bigText("This is big text This is big text This is big text This is big text ").setSummaryText("#Breaking News"))
+            setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            setContentIntent(pendingIntent)
+            setAutoCancel(true)
+        }
+        val notifManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val mChannel = NotificationChannel(channelId, "Tech News", NotificationManager.IMPORTANCE_DEFAULT).apply {
+                description = "Daily tech news update"
+            }
+            notifManager.createNotificationChannel(mChannel)
+        }
+        val rn = Random.nextInt(1000, 10000)
+        notifManager.notify(rn, builder.build())
+
+    } // showNotificaiton
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
