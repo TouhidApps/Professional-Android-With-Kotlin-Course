df7dfa99ecc2e67f5cb9968e2d7fc895a45d7b75
Touhidul Islam <touhidapps@gmail.com>
2024-03-24 14:48:18 +0800

    file upload with retrofit added

--- 041_Retrofit-with-Coroutine-File-Upload/app/src/main/java/com/touhidapps/retrofitwithcoroutine/model/ResultModel.kt
+++
@@ -1,9 +0,0 @@
-package com.touhidapps.retrofitwithcoroutine.model
-
-import com.google.gson.annotations.SerializedName
-
-data class ResultModel (
-
-  @SerializedName("result" ) var result : String? = null
-
-)
